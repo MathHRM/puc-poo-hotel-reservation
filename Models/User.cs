8693b81529fdae4b7a3c3346cec034c775c5fbe6
Matheus <matheushrm614@gmail.com>
2025-06-06 21:00:22 -0300

    add users table

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Document { get; set; } 
+    }
+}
